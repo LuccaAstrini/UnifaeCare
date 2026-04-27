5c7f1b83e38154443cb81fa20dcd786a56391bf7
Mateus Ferrarez <mateusferrarez16@gmail.com>
2026-04-27 19:44:29 -0300

    create the stack navigation

--- UniCare/App.js
+++ UniCare/App.js
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 import ResetPasswordScreen from './src/screens/resetPasswordScreen';
 import RecuperarSenha from './src/screens/recoverPasswordScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from './src/screens/loginScreens';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  
-  
   return (
-    RecuperarSenha()
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="LoginView" screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="LoginView" component={LoginScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="RecoverPasswordView" component={RecuperarSenha} options={{ headerShown: false }} />
+          <Stack.Screen name="ResetPasswordView" component={ResetPasswordScreen} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
